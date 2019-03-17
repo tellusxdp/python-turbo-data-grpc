2ae39b30568de0d288c0760c74e2498225bcfc03
chibiegg <chibiegg@chibiegg.net>
2019-03-17 15:08:23 +0900

    ipcPS.py互換クライアント

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='grpc-turbodata',
+    version='0.0.1',
+    description='package for Turbo Data gRPC Client',
+    author='Yota EGUSA',
+    author_email='y-egusa@sakura.ad.jp',
+    url='https://github.com/tellusxdp/python-turbo-data-grpc',
+    packages=["grpcturbodata"],
+    include_package_data=True,
+)
